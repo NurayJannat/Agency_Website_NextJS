3c788faa239ecbe427942c0615a095bf46a07e8d
NurayJannat <nurayjannat49@gmail.com>
2022-06-27 00:30:48 +0600

    first commit

---
+++ agency_website/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Navbar from "../components/Navbar"
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+      homepage
+      
+    </div>
+  )
+}
